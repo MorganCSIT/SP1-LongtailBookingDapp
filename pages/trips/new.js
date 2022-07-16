558445461453381f7761877639ce831a8d2366f0
morgan kieffer <morgankieffer@gmail.com>
2022-07-16 13:01:02 +0700

    added error handling and loading icon on "create" button

--- pages/trips/new.js
+++ pages/trips/new.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Button, Input } from "semantic-ui-react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
@@ -7,21 +7,32 @@ import web3 from "../../ethereum/web3";
 class TripNew extends Component {
   state = {
     boatPrice: "",
+    errorMessage: "",
+    loading: false,
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods.createTrip(this.state.boatPrice).send({
-      from: accounts[0],
-    });
+
+    this.setState({ loading: true, errorMessage: "" });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods.createTrip(this.state.boatPrice).send({
+        from: accounts[0],
+      });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
   };
 
   render() {
     return (
       <Layout>
         <h3>Create a new trip</h3>
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Price of the trip</label>
             <Input
@@ -33,7 +44,10 @@ class TripNew extends Component {
               }
             />
           </Form.Field>
-          <Button primary>Create!</Button>
+          <Message error header="Opps!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>
+            Create!
+          </Button>
         </Form>
       </Layout>
     );
